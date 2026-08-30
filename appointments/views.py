2a495cd19f2304d67bcb01e93aa08ad1d332a34e
amirhosseineilat <amirhosseineilat@gmail.com>
2026-08-30 20:28:36 +0330

    Implement appointment booking and cancellation views with success messages

--- appointments/views.py
+++ appointments/views.py
@@ -1,16 +1,59 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404,redirect
+from django.views import View
+from django.views.generic import ListView, DetailView
+from .models import Appointment
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
+
 class AppointmentListView(ListView):
-    pass
+    model = Appointment
+    template_name = "appointments/appointment_list.html"
+    context_object_name = "appointments"
 
-class AppointmentBokk():
-    pass
 
-class AppointCancel():
-    pass
+class AppointmentBookView(LoginRequiredMixin, View):
 
-class AppointmentDetail():
-    pass 
+    def post(self, request, pk):
+        appointment = get_object_or_404(
+            Appointment,
+            pk=pk,
+            user__isnull=True
+        )
 
+        appointment.user = request.user
+        appointment.save()
+
+        messages.success(request, "Appointment booked successfully.")
+
+        return redirect(
+            "appointment_detail",
+            pk=appointment.pk
+        )
+
+
+class AppointmentCancelView(LoginRequiredMixin, View):
+
+    def post(self, request, pk):
+        appointment = get_object_or_404(
+            Appointment,
+            pk=pk,
+            user=request.user
+        )
+
+        appointment.user = None
+        appointment.save()
+
+        messages.success(request, "Appointment cancelled successfully.")
+
+        return redirect(
+            "appointment"
+        )
+
+
+class AppointmentDetail(DetailView):
+    model = Appointment
+    template_name = "appointments/appointment_book.html"
+    context_object_name = "appointment"
