9f3238875a202ef448c3db5135f40a10579be3d0
amirhosseineilat <amirhosseineilat@gmail.com>
2026-08-30 19:45:10 +0330

    Add initial appointment views and update doctors views structure

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class AppointmentListView(ListView):
+    pass
+
+class AppointmentBokk():
+    pass
+
+class AppointCancel():
+    pass
+
+class AppointmentDetail():
+    pass 
+
