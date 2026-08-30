fe3f4d8f11d6433c43624c3f822fb91577b72e0f
Zeinab Soofi <z.soofi.n@gmail.com>
2026-08-30 22:35:59 +0330

    Add Comment model, form, and view for user feedback on doctors

--- doctors/models.py
+++ doctors/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -16,6 +17,9 @@ class Comment(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
+    rating = models.PositiveBigIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],
+        default=5
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- doctors/views.py
+++ doctors/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Doctor
 from django.views.generic import (
     DetailView,
     ListView,
+    CreateView
 )
 # Create your views here.
 
@@ -16,3 +18,6 @@ class DoctorListView(ListView):
     model = Doctor
     template_name = "doctors/doctor_list.html"
     context_object_name = "doctors"
+    
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    pass
