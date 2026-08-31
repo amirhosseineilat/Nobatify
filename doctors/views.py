e82b3cd7e494096360e4043e64e8d05a35561f6d
ali_balochi <alibalochi1910@gmail.com>
2026-08-31 19:39:51 +0330

    Refactor views.py to improve code organization and add SearchDoctorView for enhanced doctor search functionality

--- doctors/views.py
+++ doctors/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Doctor
-from django.views.generic import (
-    DetailView,
-    ListView,
-    CreateView
-)
+from django.views.generic import DetailView, ListView, CreateView
+from .service import DoctorService
+
 # Create your views here.
+
 
 class DoctorDetailView(DetailView):
     model = Doctor
     tmplate_name = "doctors/doctor_detail.html"
     context_object_name = "doctor"
-    
-    
+
+
 class DoctorListView(ListView):
     model = Doctor
     template_name = "doctors/doctor_list.html"
     context_object_name = "doctors"
-    
+
+
 class CommentCreateView(LoginRequiredMixin, CreateView):
     pass
+
+
+class SearchDoctorView(ListView):
+    template_name = "doctors/doctor_list.html"
+    context_object_name = "doctors"
+
+    def get_queryset(self):
+        q = self.request.GET.get("q")
+        doctors = DoctorService.search(q)
+        return doctors
