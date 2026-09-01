0a30f131d871559bfe61deffb630f5dd04d5066b
amirhosseineilat <amirhosseineilat@gmail.com>
2026-09-01 23:32:49 +0330

    Enhance appointment listing with doctor filtering and context; update templates for improved user experience

--- appointments/views.py
+++ appointments/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
+
+from doctors.models import Doctor
 from .models import Appointment
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -12,6 +14,24 @@ class AppointmentListView(ListView):
     model = Appointment
     template_name = "appointments/appointment_list.html"
     context_object_name = "appointments"
+
+    def get_queryset(self):
+        queryset = Appointment.objects.select_related(
+            "doctor"
+        )
+        doctor_id = self.request.GET.get("doctor")
+        if doctor_id:
+            queryset = queryset.filter(doctor_id=doctor_id)
+        return queryset.order_by("date", "time")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        doctor_id = self.request.GET.get("doctor")
+        if doctor_id:
+            context['selected_doctor'] = Doctor.objects.filter(pk=doctor_id).first()
+            
+        return context
+
 
 
 class MyAppointmentListView(ListView):
