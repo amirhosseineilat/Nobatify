c837ae17097fdbfaaa82e94cba9b6671c7281a43
ali_balochi <alibalochi1910@gmail.com>
2026-09-02 14:49:34 +0330

    remove status in appointment admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -5,6 +5,6 @@ from .models import Appointment
 # Register your models here.
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ("doctor", "patient", "time_slot", "status", "created_at")
+    list_display = ("doctor", "patient", "time_slot", "created_at")
     search_fields = ("doctor__first_name", "doctor__last_name", "patient__username")
-    list_filter = ("status", "created_at")
+    list_filter = ("created_at",)
