c9e7aba216966a47dede89e2e7127c6ef81f96b7
amir <amir@gmail.com>
2026-09-03 08:50:41 +0430

    adding form and urls path and view form login of the admin user

---
+++ admindashboard/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import AdminLoginView
+
+
+urlpatterns = [
+    path("login/",AdminLoginView.as_view(),name="admin_login"),
+]
--- admindashboard/views.py
+++ admindashboard/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DeleteView,UpdateView,DetailView
+from django.contrib.auth.views import LoginView
+from .forms import AdminLoginForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class AdminLoginView(LoginView):
+
+	template_name = 'socialaccount/login.html'
+	authentication_form = AdminLoginForm
+	success_url = reverse_lazy('admin_dashboard')
