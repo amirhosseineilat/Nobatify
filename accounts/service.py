68974205eae037f96f1e53519491b23921ef2a31
ali_balochi <alibalochi1910@gmail.com>
2026-09-03 09:29:39 +0330

    impliment message for login and signup

--- accounts/service.py
+++ accounts/service.py
@@ -1,13 +1,12 @@
 from .models import Otp
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
+from django.contrib import messages
 from django.utils.timezone import now
 from utils.notifications import Sender
 
-
 class AccountService:
     @staticmethod
-    def request_password_reset(user_email, sender: Sender):
+    def request_password_reset(request,user_email, sender: Sender):
         try:
             user = User.objects.get(email=user_email)
             otp = Otp()
@@ -15,14 +14,19 @@ class AccountService:
             message = f"Your OTP code is: {otp_code}"
             sender.send_notification(user, message)
             print("OTP sent successfully to", user.email)
-
+            messages.success(request,"otp send succesfuly")
+            return True
         except User.DoesNotExist:
             print("User not found")
-            return
+            messages.error(request,"user not found ")
+            return False
         except Exception as e:
+            messages.error(request,"send email faild try again")
             print("Failed to send OTP:", str(e))
+            return False
 
-    def validate_otp(otp_code, purpose):
+
+    def validate_otp(request,otp_code, purpose):
         try:
             otp: Otp = Otp.objects.get(
                 code=otp_code, expire_time__gte=now(), is_used=False, purpose=purpose
@@ -31,4 +35,5 @@ class AccountService:
             otp.save()
             return True, otp.user
         except Otp.DoesNotExist:
+            messages.error(request,"otp invalid")
             return False, None
